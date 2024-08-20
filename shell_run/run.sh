5a8d75ac3276749f334bca9f415b18f98a6546a3
Hao Liang <haoliang2023@hotmail.com>
2024-08-21 03:14:57 +0800

    Update run.sh

--- shell_run/run.sh
+++ shell_run/run.sh
@@ -3,6 +3,6 @@
 python train.py --train_dir ./data_split/One_train.txt --test_dir ./data_split/One_test.txt
 
 # Testing
-#    you can find the results in the "vis_resuls" and "output_results" folders
+#    you can find the results in the "vis_results" and "output_results" folders
 #    update the path of the pre-trained model to test a different model
-python test.py --ckpt ./save_models/08-20-23-06/last.pt --test_dir ./data/One_test.txt
+python test.py --ckpt ./save_models/08-21-00-56/last.pt --test_dir ./data_split/One_test.txt
